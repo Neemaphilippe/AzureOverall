55ba1e52a010bd6c24c163885ac9d5fe01f43271
Neema Philippe <neemaphilippe@pursuit.org>
2020-03-30 23:02:44 -0400

    Updated recipe model

--- AzureOverall/AzureOverall/Models/RecipeModel.swift
+++ AzureOverall/AzureOverall/Models/RecipeModel.swift
@@ -11,40 +11,25 @@ import Foundation
 // MARK: - Recipe
 
 struct AllRecipeInfo: Codable {
-    let recipe: [Recipe]
+    let results: [RecipeResult]
     
-    static func getRecipes(from jsonData: Data) throws -> [Recipe]? {
+    static func getRecipes(from jsonData: Data) throws -> [RecipeResult]? {
         do {
             let response = try JSONDecoder().decode(AllRecipeInfo.self, from: jsonData)
-            return response.recipe
+            return response.results
         } catch {
             print(error)
             return []
         }
     }
-}
-
-struct Recipe: Codable {
-    let results: [RecipeResult]
-    let baseURI: String
-    let offset, number, totalResults, processingTimeMS: Int
-    let expires: Int
-    let isStale: Bool
-
-    enum CodingKeys: String, CodingKey {
-        case results
-        case baseURI = "baseUri"
-        case offset, number, totalResults
-        case processingTimeMS = "processingTimeMs"
-        case expires, isStale
-    }
+    
 }
 
 // MARK: - Result
 struct RecipeResult: Codable {
-    let id: Int
-    let title: String
-    let readyInMinutes, servings: Int
-    let image: String
-    let imageUrls: [String]
+    let id: Int?
+    let title: String?
+    let readyInMinutes, servings: Int?
+    let image: String?
+    let imageUrls: [String]?
 }
